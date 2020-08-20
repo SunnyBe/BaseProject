de9a09ecde73246f9f2e1692e0e5bbf602fdbfc0
Zistus Ndu <sunday@ajocard.com>
2020-08-20 01:00:15 +0100

    Fixed build failure in app module when Hilt was implemented

---
+++ core/src/main/java/com/zistus/core/di/module/AppModuleDependencies.kt
@@ -0,0 +1,11 @@
+package com.zistus.core.di.module
+
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@EntryPoint
+@InstallIn(ApplicationComponent::class)
+interface AppModuleDependencies {
+
+}
